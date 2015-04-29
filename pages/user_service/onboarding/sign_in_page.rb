1f3891a35b8102132a85e5d5ef3c1ba33f9d66f0
Aaron Humerickhouse <aaron.humerickhouse@sportngin.com>
2015-04-29 12:11:54 -0500

    Fixing sign in

--- pages/user_service/onboarding/sign_in_page.rb
+++ pages/user_service/onboarding/sign_in_page.rb
@@ -4,13 +4,13 @@ module UserService
   module Page
     class SignIn < UserService::Page::BasePage
       page_url /https:\/\/user.stage.ngin-staging.com/
-      title 'Sport Ngin - Sign In'
+      title 'Sport Ngin'
       expected_element 'input', {id: 'user_login'}
 
       text_field(:username, id: 'user_login')
       text_field(:password, id: 'user_password')
-      #input(:sign_in, css: 'div.form-actions > input')
-      input(:sign_in, css: 'form footer input')
+      input(:sign_in, css: 'div.form-actions > input')
+      #input(:sign_in, css: 'form footer input')
     end
   end
 end
